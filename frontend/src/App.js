748b4ed7c09a11ffdc1969ab913c1c656ead68bd
moneyforey <moneyforey786@gmail.com>
2023-02-28 15:14:56 +0530

    signup login pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import AllRoutes from './componets/AllRoutes';
+import Navbar from './componets/Navbar';
 
 function App() {
   return (
     <div className="App">
-      
+        <Navbar/>
+        <AllRoutes/>
     </div>
   );
 } 
--- frontend/src/componets/AllRoutes.jsx
+++ frontend/src/componets/AllRoutes.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Login from '../pages/Login';
+import Signup from '../pages/Signup';
 
 const AllRoutes = () => {
     return (
-        <div>
-            
-        </div>
+        <Routes>
+            <Route path='/signup' element={<Signup/>}/>
+            <Route path='/login' element={<Login/>}/>
+        </Routes>
     );
 };
 
