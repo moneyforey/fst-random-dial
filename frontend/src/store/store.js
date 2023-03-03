f364120f731a74eb4e7223e979363cdda1ab3022
moneyforey <moneyforey786@gmail.com>
2023-03-03 13:10:18 +0530

    userReducer

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,8 +1,9 @@
 import {applyMiddleware, combineReducers,legacy_createStore} from 'redux';
 import thunk from'redux-thunk'
+import userReducer from './user/user.reducer';
 
 const rootReducer = combineReducers({
-    
+    user:userReducer
 })
 
 export const store = legacy_createStore(rootReducer,applyMiddleware(thunk));
--- frontend/src/store/user/user.reducer.js
+++ frontend/src/store/user/user.reducer.js
@@ -1,20 +1,41 @@
-
-const token = localStorage.getItem('token') || null
+import { user_login, user_logout, user_signup } from "./user.types";
+const token = localStorage.getItem('token') || null;
 
 const initState ={
+    message:'',
     token: token,
     isAuth:false,
-    
-}
-const userReducer = (state=initState,{type,payload})=>{
-      switch (type) {
-        case value:
-            
-            break;
-      
-        default:
-            break;
-      }
 }
 
+const userReducer = (state=initState,{type,payload})=>{
+      switch (type) {
+        case user_signup:{
+            return {
+                ...state,
+                 message:'Register Successfully'
+            }
+        }
+        case user_login:{
+            localStorage.setItem('token',payload);
+            return{
+                ...state,
+                message:'login successfully',
+                token:payload,
+                isAuth:true
+            }
+        }     
+        case user_logout:{
+            localStorage.removeItem('token');
+            return{
+                ...state,
+                message:'logout successfully',
+                token:null,
+                isAuth:false
+            }
+        }
+        default:{
+            return state
+        }
+      }
+}
 export default userReducer
